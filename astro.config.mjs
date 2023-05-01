f469c366beb3ecaee8126f9e18759a873716db15
BuildTools <unconfigured@null.spigotmc.org>
2023-05-01 16:02:00 +0200

    Added Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        drafts: true,
-    },
+  markdown: {
+    drafts: true
+  },
+  site: 'https://marni-markdown.netlify.app',
+  integrations: [sitemap()]
 });
